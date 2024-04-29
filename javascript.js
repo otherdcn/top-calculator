cc1aaf04f280978b6745120c93333cc5763ed981
otherdcn <danny.c.ntwari@gmail.com>
2024-04-29 18:49:49 +0200

    Add JS function to populate and clear display

--- javascript.js
+++ javascript.js
@@ -35,3 +35,32 @@ function operate(operator,numOne,numTwo) {
 
   return result;
 }
+
+const keypad = document.querySelector(".keypad");
+const displayContent = document.querySelector(".contents");
+const allOperationKeys = keypad.querySelectorAll(".operation-key");
+const allActionKeys = keypad.querySelectorAll(".action-key");
+const allNumberKeys = keypad.querySelectorAll(".number-key");
+let operandOne = null;
+let operandTwo = null;
+let operator;
+let result;
+
+allNumberKeys.forEach((numberKey) => {
+  numberKey.addEventListener("click", () => {
+    displayContent.textContent += numberKey.textContent;
+  });
+});
+
+allActionKeys.forEach((actionKey) => {
+  actionKey.addEventListener("click", () => {
+    if (actionKey.textContent === "AC") {
+      console.log("All Cleared Clicked")
+      displayContent.textContent = '';
+      operandOne = null;
+      operandTwo = null;
+      operator = '';
+      result = 0;
+    }
+  });
+})
