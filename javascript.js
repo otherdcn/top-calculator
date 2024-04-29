cf0f6295b700de394c1f5f1e2132b8cdf46940fb
otherdcn <danny.c.ntwari@gmail.com>
2024-04-29 16:18:35 +0200

    Add  JS function that determines math operation
    The function receives three arguments - operator, and two number operands
    - most important being the operator variable that determines which of the
    math operations to execute (add, subtract, multiply, divide).

--- javascript.js
+++ javascript.js
@@ -14,3 +14,24 @@ function divide(a,b) {
   if (b === 0) return 'nah';
   return a / b;
 }
+
+function operate(operator,numOne,numTwo) {
+  let result;
+
+  switch(operator) {
+    case '+':
+      result = add(numOne,numTwo);
+      break;
+    case  '-':
+      result = subtract(numOne,numTwo);
+      break;
+    case  '*':
+      result = multiply(numOne,numTwo);
+      break;
+    case  '/':
+      result = divide(numOne,numTwo);
+      break;
+  }
+
+  return result;
+}
