aaefd09cded7e2d0ce0712d469e1c62b83e79815
otherdcn <danny.c.ntwari@gmail.com>
2024-04-29 16:14:38 +0200

    Add basic arithmetic JS function implementations

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Outside lookin in!")
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  if (b === 0) return 'nah';
+  return a / b;
+}
