0494a022f55a65d7ff3b2696c08a22676065b8b6
otherdcn <danny.c.ntwari@gmail.com>
2024-05-01 19:32:11 +0200

    Add comments to increase readability

--- javascript.js
+++ javascript.js
@@ -7,14 +7,27 @@ const allOperationKeys = keypad.querySelectorAll(".operation-key");
 const allActionKeys = keypad.querySelectorAll(".action-key");
 const allNumberKeys = keypad.querySelectorAll(".number-key");
 
-let operandOne = null;
-let operandTwo = null;
-let numberClicked = '';
-let prevValues = [''];
-let positiveOrNegative = '+';
-let operator = null;
-let multipleOperands = true;
-let result;
+// The two variables will hold the values either side of the operator
+let operandOne = null; // First value before operator (+, -, *, /, %) is clicked
+let operandTwo = null; // second value before operator (+, -, *, /, %) is clicked
+
+/*
+Clipped from: https://www.theodinproject.com/lessons/foundations-calculator
+
+You press a number button (12), followed by an operator button (+), a second number button (7), and finally a second operator button (-). Your calculator should then do the following: first, evaluate the first pair of numbers (12 + 7), second, display the result of that calculation (19), and finally, use that result (19) as the first number in your new calculation, along with the next operator (-). 
+
+Continuous chaining instead of evaluating more than a single pair of numbers at a time.
+*/
+let multipleOperands = true; // This will enable continuous chaining
+
+// The numbers that hold the current input from the display
+let numberClicked = ''; // Concatenation of the currently clicked number
+let prevValues = ['']; // This will be used to revert back if user clicks on backspace
+let positiveOrNegative = '+'; // Will be used to negate numbers if toggled/clicked
+
+let operator = null; // This will store the math operation clicked (+, -, *, /, %)
+
+let result; // The reuslt of each operation
 
 function add(a,b) {
   return a + b;
@@ -39,6 +52,8 @@ function percentageOf(a,b) {
 
 function operate(operator,numOne,numTwo) {
   let result;
+
+  // Convert to number type especially for add function
   numOne = Number(numOne);
   numTwo = Number(numTwo);
 
@@ -60,10 +75,13 @@ function operate(operator,numOne,numTwo) {
       break;
   }
 
+  // Return final result depending on whether it's a divide by zero response or floating point
   result = (result === 'can\'t') ? 'Can\'t divide by zero' : Number(result.toFixed(5));
+  
   resultsDisplayPane.textContent = result;
+
+  // use the result as the first number in your new calculation, enabling continuous chaining
   operandOne = result;
-  return result;
 }
 
 function listenForNumberClicks() {
@@ -71,14 +89,19 @@ function listenForNumberClicks() {
     numberKey.addEventListener("click", () => {
 
       if (numberKey.textContent === "." && numberClicked.includes(".")) {
-        // Disbale if the key "." is already included in the number
+        // Exit function if the key "." is already included in the number
         return;
       }
 
       numberClicked += numberKey.textContent;
+
+      // Make the number negative or positive
       if (positiveOrNegative === "-" && numberClicked > 0) numberClicked = -numberClicked;
       if (positiveOrNegative === "+" && numberClicked < 0) numberClicked = -(numberClicked);
+
+      // Add current number just in case program need to revert to it if user click backspace
       prevValues.push(numberClicked);
+
       resultsDisplayPane.textContent = numberClicked;
     });
   });
@@ -87,13 +110,21 @@ function listenForNumberClicks() {
 function listenForOperationClick() {
   allOperationKeys.forEach((operationKey) => {
     operationKey.addEventListener("click", () => {
+
+      // When user clicks an operation key, call setOperands() function and receive return result
       if (setOperands(multipleOperands)) {
-        result = operate(operator,operandOne,operandTwo);
+        // If setOperands() return true, then immediately operate, despit the "=" key not click
+        // This enables continuous chaining
+        operate(operator,operandOne,operandTwo);
       }
+
 
       operator = operationKey.textContent;
       operationDisplayPane.textContent = operator;
+
       resetValues(true);
+
+      // Set to true if user tries continuous chaining of operands and operators before clicking "=" sign
       multipleOperands = true;
     });
   });
@@ -107,25 +138,28 @@ function listenForActionClicks() {
       } else if (actionKey.textContent === "=") {
 
         if (!operandTwo && !operandOne) {
-          // If user clicks "=" sign before entering operands
+          // Exit function if user clicks "=" sign before entering all or second operands
           return;
         }
 
-        // Keep 'multipleOperands = false' here to ensure continuous operation after user clicked '=' sign.
-        // The previous result will be used as operandOne if the user immediately clicks another operation key.
+        // The previous result will be used as operandOne if the user immediately clicks 
+        // another operation key after the "=" key
+        // Therefore set to false to wait for a number to be set to operandTwo
         multipleOperands = false;
         setOperands(multipleOperands);
 
         operationDisplayPane.textContent = actionKey.textContent;
 
-        result = operate(operator,operandOne,operandTwo);
+        operate(operator,operandOne,operandTwo);
+
+        // Only reset numberClicked, prevValues, positiveOrNegative
         resetValues(true);
       } else if (actionKey.textContent === "x") {
-        prevValues.pop();
+        prevValues.pop(); // Removes last number, which is the current number in display
 
-        if (prevValues[0] === undefined) prevValues[0] = '';
+        if (prevValues[0] === undefined) prevValues[0] = ''; // Just incased we popped the last element
         
-        resultsDisplayPane.textContent = prevValues[prevValues.length-1];
+        resultsDisplayPane.textContent = prevValues[prevValues.length-1]; // Set previous number to the display
         numberClicked = prevValues[prevValues.length-1];
       } else if (actionKey.textContent === "±") {
         toggleNumberSign();
@@ -141,34 +175,40 @@ function toggleNumberSign() {
   if (positiveOrNegative === positive) {
     positiveOrNegative = negative;
 
+    // Despite the numbers entered previous were probably positive we need to
+    // make every element negative just incase the user backspaces
     prevValues = prevValues.map((number) => {
       return -number;
     });
   } else {
     positiveOrNegative = positive;
 
+    // Despite the numbers entered previous were probably negatve we need to
+    // make every element positive just incase the user backspaces
     prevValues = prevValues.map((number) => {
       return -(number);
     });
   }
 
   numberClicked = prevValues[prevValues.length-1];
-  resultsDisplayPane.textContent = numberClicked;
+  resultsDisplayPane.textContent = numberClicked; // Display same value in display after positive/negative toggle
 }
 
 function setOperands(multipleOperands = false) {
+  // This will allow the program to operate even if user did 
+  // not click "=" sign, allowing continuous chaining, when true
   let chainOperations = false;
 
   if (operandOne && multipleOperands) {
     operandTwo = resultsDisplayPane.textContent;
-    chainOperations = true;
+    chainOperations = true; // Go ahead and chain operands and operators before clicking "=" key
   } else if (operandOne) {
     operandTwo = resultsDisplayPane.textContent;
   } else {
     operandOne = resultsDisplayPane.textContent;
   }
 
-  resultsDisplayPane.textContent = '';
+  resultsDisplayPane.textContent = ''; // Display empty display for the user to enter operandTwo
   return chainOperations;
 }
 
